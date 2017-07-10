e72ee618870f110abbf076cdea00fb7a8144c122
Alma Niu <aniu@scu.edu>
2017-07-09 20:28:09 -0700

    Update SudokuSolver.cpp

--- SudokuSolver.cpp
+++ SudokuSolver.cpp
@@ -12,17 +12,17 @@ The puzzle before the program compiles is shown below.
 000   419   005
 000   080   079
 Puzzle after compilation
-530   070   000
-600   195   000
-098   000   060
+534   678   912
+672   195   348
+198   342   567
 
-800   060   003
-400   803   001
-700   020   006
+859   761   423
+426   853   791
+713   924   856
 
-060   000   280
-000   419   005
-000   080   079
+961   537   284
+287   419   635
+345   286   179
 Program ended with exit code: 0
 */
 #include <stdio.h> 
